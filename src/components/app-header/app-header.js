c4ae1a38498a324de09f4b3e546fef42bd396483
Evgeniya-code <65096372+Evgeniya-code@users.noreply.github.com>
2020-11-11 21:40:01 +1300

    adding functionality using setState

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,11 +3,11 @@ import React from 'react';
 import './app-header.css';
 
 
-const AppHeader = () => {
+const AppHeader = ({liked, allPosts}) => {
     return (
         <div className="app-header d-flex">
             <h1> Melissa van Rensburg</h1>
-            <h2> 5 tweets 0 likes</h2>
+            <h2>{allPosts} tweets {liked} likes</h2>
         </div>
     )
 }
